84c1e591f195b40db106ee919b943f90246c5530
Matt Fischer <mattfischer84@gmail.com>
2017-02-19 17:13:43 -0600

    Fix radiance calculations

--- raytrace/Trace/Lighter/Indirect.cpp
+++ raytrace/Trace/Lighter/Indirect.cpp
@@ -31,19 +31,20 @@ Object::Radiance Indirect::light(const Trace::Intersection &intersection, Trace:
 
 	std::vector<IrradianceCache::Entry> entries = irradianceCache.lookup(point, normal);
 	if (entries.size() > 0) {
-		Object::Radiance radiance;
+		Object::Radiance incidentRadiance;
 		float den = 0;
 		for (const IrradianceCache::Entry &entry : entries) {
 			float weight = irradianceCache.weight(entry, point, normal);
 			if (std::isinf(weight)) {
-				return radiance * albedo;
+				return entry.radiance * albedo * brdf.lambert() / (2 * M_PI);
 			}
 			Math::Vector cross = Math::Vector(normal % entry.normal);
 			Object::Radiance rotRad(entry.rotGradR * cross, entry.rotGradG * cross, entry.rotGradB * cross);
-			radiance += (entry.radiance + rotRad ) * weight;
+			incidentRadiance += (entry.radiance + rotRad ) * weight;
 			den += weight;
 		}
-		return radiance * albedo / den;
+		incidentRadiance = incidentRadiance / den;
+		return incidentRadiance * albedo * brdf.lambert() / (2 * M_PI);
 	}
 
 	return Object::Radiance();
@@ -51,30 +52,26 @@ Object::Radiance Indirect::light(const Trace::Intersection &intersection, Trace:
 
 bool Indirect::prerender(const Trace::Intersection &intersection, Trace::Tracer &tracer) const
 {
-	const Math::Point point(intersection.point());
-	const Math::Vector normal(intersection.normal());
-	const Object::Color &albedo = intersection.primitive()->surface().albedo().color(intersection.objectPoint());
-	const Object::Brdf::Base &brdf = intersection.primitive()->surface().brdf();
-	const Math::Vector &outgoingDirection = -intersection.ray().direction();
+	const Math::Point &point = intersection.point();
+	const Math::Normal &normal = intersection.normal();
 	Trace::IrradianceCache &irradianceCache = tracer.renderData().irradianceCache;
 
-	std::vector<IrradianceCache::Entry> entries = irradianceCache.lookup(point, intersection.normal());
+	std::vector<IrradianceCache::Entry> entries = irradianceCache.lookup(point, normal);
 	if (entries.size() > 0) {
 		return false;
 	}
 
 	Math::Vector x, y;
-	Utils::orthonormalBasis(normal, x, y);
+	Utils::orthonormalBasis(Math::Vector(normal), x, y);
 
 	float mean = 0;
-	Object::Radiance cacheRadiance;
 	Object::Radiance radiance;
 	Math::Vector rotGradR;
 	Math::Vector rotGradG;
 	Math::Vector rotGradB;
 	for (int i = 0; i < mIndirectSamples; i++) {
 		Math::Vector v = Utils::sampleHemisphereCosineWeighted(i, mIndirectSamples, mRandomEngine);
-		Math::Vector incidentDirection = x * v.x() + y * v.y() + normal * v.z();
+		Math::Vector incidentDirection = x * v.x() + y * v.y() + Math::Vector(normal) * v.z();
 
 		Trace::Ray ray(intersection.point(), incidentDirection, intersection.ray().generation() + 1);
 		Trace::IntersectionVector::iterator begin, end;
@@ -91,28 +88,21 @@ bool Indirect::prerender(const Trace::Intersection &intersection, Trace::Tracer 
 			Trace::Intersection intersection2 = *begin;
 
 			mean += 1 / intersection2.distance();
-			Object::Radiance incidentRadiance = mDirectLighter.light(intersection2, tracer);
-			Object::Radiance c = incidentRadiance * brdf.lambert();
-			cacheRadiance += c / (2 * M_PI);
+			Object::Radiance incidentRadiance = mDirectLighter.light(intersection2, tracer) * 2 * M_PI / mIndirectSamples;
 
-			rotGradR = rotGradR + perpTrans * tan * c.red();
-			rotGradG = rotGradG + perpTrans * tan * c.green();
-			rotGradB = rotGradB + perpTrans * tan * c.blue();
-			radiance += brdf.radiance(incidentRadiance, incidentDirection, intersection.normal(), outgoingDirection, albedo);
+			radiance += incidentRadiance;
+			rotGradR = rotGradR + perpTrans * tan * incidentRadiance.red();
+			rotGradG = rotGradG + perpTrans * tan * incidentRadiance.green();
+			rotGradB = rotGradB + perpTrans * tan * incidentRadiance.blue();
 		}
 	}
-
-	radiance = radiance * 2 * M_PI / mIndirectSamples;
-	rotGradR = rotGradR * M_PI / mIndirectSamples;
-	rotGradG = rotGradG * M_PI / mIndirectSamples;
-	rotGradB = rotGradB * M_PI / mIndirectSamples;
 
 	if (mean > 0) {
 		IrradianceCache::Entry newEntry;
 		newEntry.point = point;
-		newEntry.normal = intersection.normal();
+		newEntry.normal = normal;
 		newEntry.radius = mIndirectSamples / mean;
-		newEntry.radiance = cacheRadiance * 2 * M_PI / mIndirectSamples;
+		newEntry.radiance = radiance;
 		newEntry.rotGradR = rotGradR;
 		newEntry.rotGradG = rotGradG;
 		newEntry.rotGradB = rotGradB;
