dac5065e55cfdfab52feb714fddf5f2995207236
Matt Fischer <mattfischer84@gmail.com>
2018-10-28 19:59:11 -0500

    Optimize Oren-Nayar BRDF

--- raytrace/Object/Brdf/OrenNayar.cpp
+++ raytrace/Object/Brdf/OrenNayar.cpp
@@ -19,11 +19,13 @@ namespace Object {
 
 		Object::Radiance OrenNayar::reflected(const Object::Radiance &incidentRadiance, const Math::Vector &incidentDirection, const Math::Normal &normal, const Math::Vector &outgoingDirection, const Object::Color &albedo) const
 		{
-			float thetaI = std::acos(std::min(1.0f, incidentDirection * normal));
-			float sinThetaI = std::sin(thetaI);
-			
-			float thetaR = std::acos(std::min(1.0f, outgoingDirection * normal));
-			float sinThetaR = std::sin(thetaR);
+			float cosThetaI = incidentDirection * normal;
+			float sinThetaI = std::sqrt(std::max(0.0f, 1 - cosThetaI * cosThetaI));
+			float tanThetaI = sinThetaI / cosThetaI;
+
+			float cosThetaR = outgoingDirection * normal;
+			float sinThetaR = std::sqrt(std::max(0.0f, 1 - cosThetaR * cosThetaR));
+			float tanThetaR = sinThetaR / cosThetaR;
 
 			float cosPhi;
 			if (sinThetaI < 0.001 || sinThetaR < 0.001) {
@@ -39,10 +41,10 @@ namespace Object {
 			float A = 1.0f - 0.5f * sigma2 / (sigma2 + 0.33f);
 			float B = 0.45f * sigma2 / (sigma2 + 0.09f);
 
-			float alpha = std::max(thetaI, thetaR);
-			float beta = std::min(thetaI, thetaR);
+			float sinAlpha = std::max(sinThetaI, sinThetaR);
+			float tanBeta = std::min(tanThetaI, tanThetaR);
 
-			return incidentRadiance * albedo * mStrength * (A + B * std::max(0.0f, cosPhi) * std::sin(alpha) * std::tan(beta)) / M_PI;
+			return incidentRadiance * albedo * mStrength * (A + B * std::max(0.0f, cosPhi) * sinAlpha * tanBeta) / M_PI;
 		}
 	}
 }
