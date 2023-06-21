7c848c2adf4883a22f531887d721a5c569485f60
Matt Fischer <mattfischer84@gmail.com>
2023-06-20 22:59:10 -0500

    Simplify WorkQueue operations

--- Render/Queued/WorkQueue.cpp
+++ Render/Queued/WorkQueue.cpp
@@ -11,43 +11,27 @@ namespace Render {
 
         WorkQueue::Key WorkQueue::getNextKey()
         {
-            while(true) {
-                int read = mRead.load(std::memory_order_acquire);
-                int newRead = read + 1;
-                if(read == mWrite.load(std::memory_order_acquire)) {
-                    return kInvalidKey;
-                }
-
-                Key key = mQueue[read];    
-                if(mRead.compare_exchange_weak(read, newRead, std::memory_order_acq_rel)) {
-                    return key;
-                }
+            int read = mRead++;
+            if(read >= mWrite) {
+                return kInvalidKey;
             }
+
+            return mQueue[read];
         }
 
         bool WorkQueue::addItem(Key key)
         {
-            while(true) {
-                int write = mWrite.load(std::memory_order_acquire);
-                int newWrite = write + 1;
-                if(write == mQueue.size()) {
-                    return false;
-                }
-
-                if(mWrite.compare_exchange_weak(write, newWrite, std::memory_order_acq_rel)) {                
-                    mQueue[write] = key;
-                    return true;
-                }
+            int write = mWrite++;
+            if(write >= mQueue.size()) {
+                return false;
             }
+            mQueue[write] = key;
+            return true;
         }
 
         int WorkQueue::numQueued()
         {
-            if(mWrite >= mRead) {
-                return mWrite - mRead;
-            } else {
-                return mQueue.size() + mWrite - mRead;
-            }
+            return mWrite - mRead;
         }
 
         void WorkQueue::clear()
