baed53ca15e83ecc28e5ebdca5ace3e05ac8f48f
Matt Fischer <mattfischer84@gmail.com>
2024-11-24 22:30:02 -0600

    Add .ply file loader

--- src/input/model_loader.rs
+++ src/input/model_loader.rs
@@ -1,6 +1,15 @@
 use crate::geo;
 use crate::object;
 
+use object::shape::TriangleMesh;
+use object::shape::TriangleMeshVertex;
+use object::shape::TriangleMeshTriangle;
+
+use ply_rs;
+use ply_rs::ply;
+use ply_rs::ply::PropertyAccess;
+
+use geo::Normal3;
 use geo::Point3;
 
 use std::io::{BufRead, BufReader};
@@ -49,9 +58,49 @@ impl ModelLoader {
         }
     }
 
+    fn load_ply(filename : String) -> Option<Box<dyn object::Shape>> {
+        if let Ok(mut file) = File::open(filename) {
+            let parser = ply_rs::parser::Parser::<ply::DefaultElement>::new();
+            let ply = parser.read_ply(&mut file);
+            if let Ok(ply) = ply {
+                let mut points = Vec::new();
+                for elem in ply.payload["vertices"].iter() {
+                    let x = elem.get_float(&String::from("x")).unwrap_or(0.0);
+                    let y = elem.get_float(&String::from("y")).unwrap_or(0.0);
+                    let z = elem.get_float(&String::from("z")).unwrap_or(0.0);
+                    points.push(TriangleMeshVertex{point: Point3::new(x, y, z)});
+                }
+
+                let mut triangles = Vec::new();
+                for elem in ply.payload["face"].iter() {
+                    let ind = elem.get_list_uchar(&String::from("vertex_indices")).unwrap_or(&[0, 0, 0]);
+                    let mut indices = [0 as usize; 3];
+            
+                    for i in 0..3 {
+                        indices[i] = ind[i] as usize;
+                    }
+
+                    let u = points[indices[1]].point - points[indices[0]].point;
+                    let v = points[indices[2]].point - points[indices[0]].point;
+                    
+                    let normal = Normal3::from_vec3(u % v);
+                    triangles.push(TriangleMeshTriangle{vertices: indices, normal: normal});
+                }
+
+                return Some(Box::new(TriangleMesh::new(points, triangles)));
+            } else {
+                return None;
+            }
+        } else {
+            return None;
+        }
+    }
+
     pub fn load(filename : String) -> Option<Box<dyn object::Shape>> {
         if filename.ends_with(".bpt") {
             return Self::load_bpt(filename);
+        } else if filename.ends_with(".ply") {
+            return Self::load_ply(filename);
         }
 
         return None;
